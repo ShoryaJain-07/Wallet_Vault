c2c1f7c169e92d3666f8a6930f089c73580baf6a
Shorya Jain <shoryajain0708@gmail.com>
2024-05-23 20:52:08 +0530

    resolve-err

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -3,7 +3,7 @@ import { signIn, useSession } from 'next-auth/react';
 import { redirect } from "next/navigation";
 import React from 'react'
 
-const Page = () => {
+const SignUp = () => {
     const session = useSession();
 
     if(session.data){
@@ -27,4 +27,4 @@ const Page = () => {
   );
 }
 
-export default Page
+export default SignUp
